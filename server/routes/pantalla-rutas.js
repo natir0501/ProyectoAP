ebfb989b3e3abe11eca49ba26f660db6b3ca43b3
Nati <nati.r0501@gmail.com>
2018-10-27 21:16:47 -0300

    Modificaciones en pantalla para asignar permisos. Pendiente control de que el objectId sea un rol

--- server/routes/pantalla-rutas.js
+++ server/routes/pantalla-rutas.js
@@ -2,6 +2,7 @@ var express = require('express');
 var api= express.Router();
 const {Pantalla}=require('../models/pantalla')
 const _ = require('lodash')
+const {ObjectID} = require('mongodb')
 
  api.get('/pantallas',(req, res)=>{
     Pantalla.find()
@@ -16,11 +17,26 @@ const _ = require('lodash')
 api.post('/pantallas', async (req,res) =>{
     
     try{
-        var pantalla = new Pantalla(_.pick(req.body,['nombre','codigo']))
+        var arrayRolesIds=req.body.roles;
+        var roles=[]
+        
+        if(arrayRolesIds){
+            arrayRolesIds.forEach(element => {
+                if(!ObjectID.isValid(element)){
+                    return res.status(404).send()
+                }else{
+                    roles.push(new ObjectID(element));
+                }
+            });
+
+        }
+        var nombre=req.body.nombre
+        var codigo=req.body.codigo
+        var pantalla = new Pantalla({nombre, codigo, roles})
         await pantalla.save()
         res.status(200).send({"mensaje":"Agregado ok"});
     }catch(e){
-        res.status(400).send({"error": e.errors.codigo.message})
+        res.status(400).send({"error": e})
     }
 })
 
@@ -39,5 +55,27 @@ api.get('/pantallas/:codigo',(req,res)=>{
         res.status(400).send()
     })    
 })
+
+api.put('/pantallas/:codigo',(req,res)=>{
+    var codigo = req.params.codigo;
+    var body= _pick(req.body,['nombre','codigo','roles']);
+    
+    Pantalla.findOneAndUpdate({
+        codigo: codigo
+    },{
+        $set:body
+    },{
+        new: true
+    }).then((pantalla)=> {
+        if(pantalla){
+            res.send({"mensaje":"Actualizado Ok"})
+        }else{
+            res.status(404).send()
+        }
+    }).catch((e)=>{
+        res.status(400).send()
+    })    
+})
+
 
  module.exports=api;
