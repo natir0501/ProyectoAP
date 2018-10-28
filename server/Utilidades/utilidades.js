17582c19f0713d4ec05e3f68f01d8c5dabd9fc6c
Nati <nati.r0501@gmail.com>
2018-10-28 20:49:19 -0300

    Modelo usuarios. Metodo GET y POST.

---
+++ server/Utilidades/utilidades.js
@@ -0,0 +1,1 @@
+//Acá irían las posiciones
---
+++ server/routes/usuario-rutas.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var api= express.Router();
+const {Usuario}=require('../models/usuario')
+const _ = require('lodash')
+
+api.get('/usuarios',(req, res)=>{
+    Usuario.find()
+    .then((usuarios)=>{
+        res.send({usuarios})
+    }),(e)=>{
+        res.status(400).send(e)
+    }
+})
+
+api.post('/usuarios', async (req,res) =>{
+    
+    try{
+        var usuario = new Usuario(_.pick(req.body,['nombre','apellido','email']))
+        await usuario.save()
+        res.status(200).send({"mensaje":"Usuario ok"});
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+module.exports=api;
