731574a1995c7a14320906a6175326e2a1c7aee9
Nati <nati.r0501@gmail.com>
2018-11-02 20:03:10 -0300

    Modificar y Agregar Conceptos de caja.

--- server/routes/conceptosCaja-rutas.js
+++ server/routes/conceptosCaja-rutas.js
@@ -14,4 +14,59 @@ api.get('/conceptosCaja',(req, res)=>{
     }
 })
 
+
+api.get('/conceptosCaja/:id', async (req,res)=>{
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)){
+        res.status(404).send(new ApiResponse({},"Código de Id inválido."))
+    }
+
+    ConceptosCaja.findOne({
+        _id: id
+    }).then((conceptosCaja)=> {
+        if(conceptosCaja){
+            res.status(200).send(new ApiResponse({conceptosCaja},''))
+        }else{
+            res.status(404).send(new ApiResponse({},"No hay datos para mostrar."))
+        }
+    }).catch((e)=>{
+        res.status(400).send(new ApiResponse({e},"Error"))
+    })    
+})
+
+api.post('/conceptosCaja', async (req,res) =>{
+    
+    try{
+        var conceptoCaja = new ConceptosCaja(_.pick(req.body,['nombre','tipo']))
+        await conceptoCaja.save()
+       
+        res.status(200).send(new ApiResponse({mensaje : 'Agregado ok'},''));
+    }catch(e){
+        res.status(400).send(new ApiResponse({},"No se pudo agregar el concepto de caja."))
+    }
+})
+
+api.put('/conceptosCaja/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['nombre','tipo']);
+
+    ConceptosCaja.findOneAndUpdate({
+        _id: id
+    }, {
+            $set: body
+        }, {
+            new: true
+        }).then((conceptoCaja) => {
+            if (conceptoCaja) {
+                res.status(200).send(new ApiResponse({mensaje : 'Modificado OK.'},''));
+            } else {
+                res.status(404).send(new ApiResponse({},"No se encontró un concepto para modificar."))
+            }
+        }).catch((e) => {
+            res.status(400).send(new ApiResponse({e},"Error al modificar. Revisar datos ingresados."))
+        })
+})
+
+
 module.exports=api;
