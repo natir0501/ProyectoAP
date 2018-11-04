6353e6f7c06a6932377801a061096925d4cee579
Nati <nati.r0501@gmail.com>
2018-11-04 15:47:16 -0300

    Al agregar categoría, creo la Caja con su saldo inicial.

--- server/routes/categoria-rutas.js
+++ server/routes/categoria-rutas.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var api= express.Router();
 const {Categoria}=require('../models/categoria')
+const {Caja}=require('../models/caja')
 const _ = require('lodash')
 const {validarId} = require('../Utilidades/utilidades')
 const {ApiResponse} = require('../models/api-response')
@@ -17,7 +18,6 @@ api.get('/categorias',(req, res)=>{
 api.post('/categorias', async (req,res) =>{
     
     try{
-        console.log('entro')
         let tesoreros=req.body.tesoreros;
         let delegados=req.body.delegados;
         let jugadores=req.body.jugadores;
@@ -28,24 +28,32 @@ api.post('/categorias', async (req,res) =>{
         let diaVtoCuota=req.body.diaVtoCuota;
         let cantidadCuotasAnuales=req.body.cantidadCuotasAnuales;
 
+        //En el body de la categoría, recibo saldo inicial
+        let saldo=req.body.saldoInicial;
+        let movimientos=[]
+
         let arrayIds = []
         
         arrayIds.push(...tesoreros)
-        console.log('hola')
         arrayIds.push(...delegados)
         arrayIds.push(...jugadores)
         arrayIds.push(...dts)
        
-        console.log('Array',arrayIds)
-        console.log('antes del if')
         if(validarId(arrayIds)){
-            console.log('en el if')
+
+            let cajaCategoria = new Caja({movimientos,saldo});
+            await cajaCategoria.save();
+            let caja= cajaCategoria._id;
+           console.log(caja);
+           
+            
             let categoria=new Categoria({nombre,valorCuota,diaGeneracionCuota,
-                diaVtoCuota,cantidadCuotasAnuales,dts,tesoreros,delegados,jugadores})
+                diaVtoCuota,cantidadCuotasAnuales,dts,tesoreros,delegados,jugadores,caja})
             await categoria.save();
            
             await categoria.asignarRoles()
-            return res.status(200).send(new ApiResponse(categoria,''));
+            return res.status(200).send('OK');
+            //return res.status(200).send(new ApiResponse(categoria,''));
         }
         res.status(400).send(new ApiResponse({},'Algunos de los usuarios no son válidos'))        
     }catch(e){
