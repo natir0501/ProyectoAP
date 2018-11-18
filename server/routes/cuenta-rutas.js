0b31ed596d26cd0a33dc12d66bc3e8a14c39f543
Nati <nati.r0501@gmail.com>
2018-11-18 12:51:59 -0300

    Ingreso de movimientos (incompleto)

--- server/routes/cuenta-rutas.js
+++ server/routes/cuenta-rutas.js
@@ -22,4 +22,50 @@ api.get('/cuenta/:_id',(req,res)=>{
     })    
 })
 
+
+api.put('/cuenta/movimientos/ingresomovimiento', async (req,res) =>{
+    
+    try{ 
+        let idCuenta=req.body._id;
+        let tipoMov=req.body.tipoMovimiento;
+        let fecha=req.body.fecha;
+        let monto=req.body.monto;
+        let concepto= req.body.concepto;
+        let comentario=req.body.comentario;
+        let usuMovimiento=req.body.usuario;
+
+        if(tipoMov==="1"){tipoMov='Ingreso'} 
+        else if(tipoMov==="2"){tipoMov='Egreso';monto=-monto}
+        else{res.status(404).send(new ApiResponse({},"Tipo de movimiento inválido"))}
+
+        let movimiento = {fecha, monto, tipoMov, concepto, comentario,usuMovimiento}
+        let cuenta= await Cuenta.findById(idCuenta).populate('movimientos').exec();
+        console.log(cuenta);
+        
+        //push del movimiento. 
+        //impactar saldo categoria.
+        console.log(movimiento);
+        
+
+        Cuenta.findOneAndUpdate({
+            _id: idCuenta
+        },{
+            $set: cuenta
+        }, {
+            new: true
+        }).then((cuenta)=>{
+            if (cuenta) {
+                res.status(200).send(new ApiResponse({cuenta}));
+            } else {
+                res.status(404).send(new ApiResponse({},"Ocurrió un error al agregar el movimiento"))
+            }
+        }).catch((e)=>{
+            res.status(400).send(new ApiResponse({},"400-Ocurrió un error al agregar el movimiento"))
+        })
+    }catch(e){
+        res.status(400).send(new ApiResponse({},`Mensaje: ${e}`))
+    }
+})
+
+
 module.exports=api;
