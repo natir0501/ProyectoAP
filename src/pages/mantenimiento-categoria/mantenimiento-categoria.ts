c5deecd1ccf60caa1ad84ddc341135f6d3858eeb
Gabriel Arce <gab.arpe@gmail.com>
2018-11-25 20:09:34 -0300

    WIP

---
+++ src/pages/mantenimiento-categoria/mantenimiento-categoria.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the MantenimientoCategoriaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-mantenimiento-categoria',
+  templateUrl: 'mantenimiento-categoria.html',
+})
+export class MantenimientoCategoriaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MantenimientoCategoriaPage');
+  }
+
+}
