1d85485d09fe65442e457e069af32ec5ba9f828c
Nati <nati.r0501@gmail.com>
2018-11-25 20:10:09 -0300

    alta usuario

---
+++ src/pages/common/alta-usuario/alta-de-usuario.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-alta-de-usuario',
+  templateUrl: 'alta-de-usuario.html'
+})
+export class AltaDeUsuarioPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
