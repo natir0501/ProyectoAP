dd7f75f59e1fbed656676a817fcc74c4691b4454
Nati <nati.r0501@gmail.com>
2018-12-10 23:07:50 -0300

    consulta eventos filtrada

--- server/routes/evento-rutas.js
+++ server/routes/evento-rutas.js
@@ -1,85 +1,75 @@
 var express = require('express');
-var api= express.Router();
-const {Evento}=require('../models/evento')
+var api = express.Router();
+const { Evento } = require('../models/evento')
 const _ = require('lodash')
-const {ApiResponse} = require('../models/api-response')
-const {ObjectID} = require('mongodb')
+const { ApiResponse } = require('../models/api-response')
+const { ObjectID } = require('mongodb')
 
-api.get('/eventos', async (req, res)=>{
-    
-    try{
-        filtros = []
+api.get('/eventos', async (req, res) => {
+
+    try {
+
+        let filtro = {}
+        if (req.query.fechaInicio && req.query.fechaFin) {
+            let fecha = { $lt: req.query.fechaFin, $gt: req.query.fechaInicio }
+            filtro = { fecha }
+        }
         if (req.query.tipoEvento) {
-            filtros.push({ 'tipoEvento': ObjectID(req.query.tipoEvento)})
-            console.log("tipoevento :" + req.query.tipoEvento);
-            
+
+            filtro = {
+                ...filtro,
+                'tipoEvento': ObjectID(req.query.tipoEvento)
+            }
         }
-        if (req.query.fechaInicio) {
-            filtros.push({ 'fecha': { $gt: req.query.fechainicio } })
-        }
-        if (req.query.fechaFin) {
-            filtros.push({ 'fecha': { $lt: req.query.fechafin } })
-        }
-        
-        if(filtros.length>0){
-            console.log("Filtros");
-            console.log(filtros[0]);
-            
-                await Evento.find(filtros[0]).then((eventos)=>{
-                res.status(200).send(new ApiResponse({eventos}))
-            })
-        }else{
-                await Evento.find().then((eventos)=>{
-                res.status(200).send(new ApiResponse({eventos}))
-            })
-        }
-    }catch (e){
+        let eventos= await Evento.find(filtro)
+        res.status(200).send(new ApiResponse({eventos}))
+    } catch (e) {
         res.status(400).send(new ApiResponse({}, `Error al obtener datos: ${e}`))
     }
 })
 
-api.get('/eventos/:id', async (req,res)=>{
+api.get('/eventos/:id', async (req, res) => {
     var id = req.params.id;
-    
-    if(!ObjectID.isValid(id)){
-        res.status(404).send(new ApiResponse({},"No se pudo obtener el evento"))
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send(new ApiResponse({}, "No se pudo obtener el evento"))
     }
 
     Evento.findOne({
         _id: id
-    }).then((evento)=> {
-        if(evento){
-            res.status(200).send(new ApiResponse({evento},''))
-        }else{
-            res.status(404).send(new ApiResponse({},"No hay datos para mostrar."))
+    }).then((evento) => {
+        if (evento) {
+            res.status(200).send(new ApiResponse({ evento }, ''))
+        } else {
+            res.status(404).send(new ApiResponse({}, "No hay datos para mostrar."))
         }
-    }).catch((e)=>{
-        res.status(400).send(new ApiResponse({},"Ocurrió un error"))
+    }).catch((e) => {
+        res.status(400).send(new ApiResponse({}, "Ocurrió un error"))
         console.log(e);
-    })    
+    })
 })
 
-api.post('/eventos', async (req,res) =>{
-    
-    try{
-        var evento = new Evento(_.pick(req.body, ['fecha','nombre','tipoEvento','tipoEvento','lugar',
-        'rival','invitados','categorias']))
+api.post('/eventos', async (req, res) => {
+
+    try {
+        var evento = new Evento(_.pick(req.body, ['fecha', 'nombre', 'tipoEvento', 'tipoEvento', 'lugar',
+            'rival', 'invitados', 'categorias']))
         await evento.save()
-       
-        res.status(200).send(new ApiResponse({evento}));
+
+        res.status(200).send(new ApiResponse({ evento }));
         console.log("agregado OK.");
-        
-    }catch(e){
-        res.status(400).send(new ApiResponse({},"No se pudo agregar el evento."))
+
+    } catch (e) {
+        res.status(400).send(new ApiResponse({}, "No se pudo agregar el evento."))
         console.log(e);
-        
+
     }
 })
 
 api.put('/eventos/:id', async (req, res) => {
     try {
         let _id = req.params.id;
-        let evento = await Evento.findOneAndUpdate({_id}, { $set: req.body })
+        let evento = await Evento.findOneAndUpdate({ _id }, { $set: req.body })
         if (!evento) {
             res.status(401).send(new ApiResponse({}, 'No fue posible actualizar el evento'))
         }
@@ -87,7 +77,7 @@ api.put('/eventos/:id', async (req, res) => {
         console.log("Actualizado ok");
     }
     catch (e) {
-        res.status(400).send(new ApiResponse({},"Ocurrió un error al intentar actualizar"))
+        res.status(400).send(new ApiResponse({}, "Ocurrió un error al intentar actualizar"))
         console.log(e);
     }
 })
