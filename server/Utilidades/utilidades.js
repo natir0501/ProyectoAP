322f071f0e4a4937fee8484d29d6ab38f901fd0e
Gabriel Arce <gab.arpe@gmail.com>
2019-01-14 00:53:23 -0300

    Fix push notifications

--- server/Utilidades/utilidades.js
+++ server/Utilidades/utilidades.js
@@ -49,12 +49,12 @@ const enviarCorreoAlta = async (usuario) => {
         service: 'gmail',
         auth: {
             type: "OAuth2",
-            user: "appcei.2018@gmail.com", 
+            user: "appcei.2018@gmail.com",
             clientId: "349297601621-s63gdr5v1ms3kb88ahe5r4glaqire5t0.apps.googleusercontent.com",
             clientSecret: "4k7UuEp__UAjcVfOsVJtZTe0",
             refreshToken: "1/1tC904dBJ2cw_-7KUdqe9qroSdRr4Zpz6maMeJEHmQY",
             accessToken: accessToken
-       }
+        }
     });
 
     let ambiente;
@@ -82,23 +82,27 @@ const enviarCorreoAlta = async (usuario) => {
     });
 }
 
-const enviarNotificacion = async (usuario,evento) => {
+const enviarNotificacion = async (usuario, evento) => {
     var headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'key=AAAAOcMNI0o:APA91bHamReBWXoEEMRMwA5B7KS8K73DEIhMNJizbIzX1IJ6Eb_LIaBFYdbXhIUHzIEutsD-8sGL4RGB2vgn086rcWqSRsVOpwv4oNwOfffjc9JDc1O1l2wwPijXX89NEZ3TxcR53_3J' 
+        'Authorization': 'key=AAAAOcMNI0o:APA91bHamReBWXoEEMRMwA5B7KS8K73DEIhMNJizbIzX1IJ6Eb_LIaBFYdbXhIUHzIEutsD-8sGL4RGB2vgn086rcWqSRsVOpwv4oNwOfffjc9JDc1O1l2wwPijXX89NEZ3TxcR53_3J'
     }
-    
-    for (let i = 1; i<usuario.tokens.length ; i++){
+
+    for (let i = 1; i < usuario.tokens.length; i++) {
         let mensaje = {
-            "notification": {
-                "title": `Nuevo evento: ${evento.nombre}`,
+            "to": usuario.tokens[i].token,
+            "data":{
+                "notification": {
+                    "title": `Nuevo evento: ${evento.nombre}`,
                     "body": `Hola ${usuario.nombre}! Has sido invitado a un nuevo evento. Por favor, consultá los detalles y confirmá asistencia. Gracias!`
-            },
-            "to" : usuario.tokens[i].token
+                }
+
+            }
+            
         }
-        
-    
-        axios.post('https://fcm.googleapis.com/fcm/send', mensaje, {headers})
+
+
+        axios.post('https://fcm.googleapis.com/fcm/send', mensaje, { headers })
             .then((res) => {
                 console.log('Envió')
             })
